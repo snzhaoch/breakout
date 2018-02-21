a5d700e0152d478b21ba21141619ff4d319505b4
snzhaoch <snzhaoch@gmail.com>
2018-02-22 00:25:04 +0800

    refactor: 将 game 重构为 class

--- game/game.js
+++ game/game.js
@@ -1,48 +1,48 @@
-var Game = function(imgs){
+class Game {
     // imgs 是一个对象，里面是图片的名称和引用路径
-    var g = {
-        imgs: {},
-    }
-
-    var canvas = document.querySelector('#id-canvas')
-    var content = canvas.getContext('2d')
-    g.canvas = canvas
-    g.content = content
-    g.drawImg = function(img){
-        g.content.drawImage(img.img, img.x, img.y)
-    }
-    g.imageFromName = function(name) {
-        return g.imgs[name]
-    }
-    g.changeScene = function(scene) {
-        g.scene = scene
-    }
-    g.clearScene = function() {
-        g.content.clearRect(0, 0, canvas.width, canvas.height)
+    constructor(imgs) {
+        this.imgs = imgs
+        this.canvas = document.querySelector('#id-canvas')
+        this.content = this.canvas.getContext('2d')
+        this.init()
     }
     // 预先载入所有图片，完成后执行程序
-    var onload_num = []
-    var names = Object.keys(imgs)
-    for (var i = 0; i < names.length; i++){
-        var name = names[i]
-        var path = imgs[name]
-        var img = new Image()
-        img.src = path
-        // 将 img 保存
-        g.imgs[name] = img
-        img.onload = function(){
-            // js 图片载入是异步事件，需要判断是否所有图片加载成功
-            onload_num.push(1)
-            if  (onload_num.length === names.length){
-                runWithScene(g.scene)
+    init() {
+        var onload_num = []
+        var names = Object.keys(this.imgs)
+        for (var i = 0; i < names.length; i++){
+            var name = names[i]
+            var path = this.imgs[name]
+            var img = new Image()
+            img.src = path
+            // 将 img 保存
+            this.imgs[name] = img
+            var _this = this
+            img.onload = function(){
+                // js 图片载入是异步事件，需要判断是否所有图片加载成功
+                onload_num.push(1)
+                if  (onload_num.length === names.length){
+                    _this.runWithScene(_this.scene)
+                }
             }
         }
     }
+    drawImg(img) {
+        this.content.drawImage(img.img, img.x, img.y)
+    }
+    imageFromName(name) {
+        return this.imgs[name]
+    }
+    changeScene(scene) {
+        this.scene = scene
+    }
+    clearScene() {
+        this.content.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    }
     // loop
-    var runloop = function(){
-        var scene = g.scene
+    runloop() {
+        var scene = this.scene
         var actions = Object.keys(scene.actions)
-        // log(actions)
         for (var i = 0; i < actions.length; i++){
             var key = actions[i]
             var action = scene.actions[key]
@@ -64,20 +64,20 @@ var Game = function(imgs){
 
         }
         // clear
-        g.clearScene()
+        this.clearScene()
         // 火球移动等动作
-        g.scene.update()
+        this.scene.update()
         // draw
-        g.scene.draw()
-        runWithScene(g.scene)
+        this.scene.draw()
+        this.runWithScene(this.scene)
     }
     // run()
-    var runWithScene = function(scene){
-        g.scene = scene
+    runWithScene(scene) {
+        this.scene = scene
+        var _this = this
         setTimeout(function(){
-            runloop()
+            _this.runloop()
         }, 1000/window.fps)
     }
-
-    return g
+    // return g
 }
